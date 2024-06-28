f70d7eba79662a8c950d29b2f22c26ae4d8cc1cf
Alisson <alissonporpino.aps@gmail.com>
2024-06-28 16:34:35 -0300

    Atualizando Models

--- models/documentos_model.py
+++ models/documentos_model.py
@@ -9,7 +9,7 @@ class DocumentosModel(settings.DBBaseModel):
     __tablename__: str = 'documents'
 
     id: str = Column(Integer, primary_key=True, autoincrement=True)
-    #num_reg: str = Column(String(100))
+    num_reg: str = Column(String(100), unique=True, nullable=False)
     nome_doc: str = Column(String(120), unique=True, nullable=False)
     #objeto: str = Column(String(120), unique=True, nullable=False)
     date_created: str = Column(String, default=datetime.now, nullable=False)
@@ -17,5 +17,5 @@ class DocumentosModel(settings.DBBaseModel):
     #requester: str = Column(String, default=datetime.now, onupdate=datetime.now, nullable=True)
     #criador: str = relationship("UsuarioModel", back_populates='artigos', lazy='joined')
     tipo: str = Column(String(255), nullable=False)
-    origem: str = Column(String(255), nullable=False)
+    setor_origem: str = Column(String(255), nullable=False)
     #destino: str = Column(String(255), nullable=False)
--- schemas/documentos_schema.py
+++ schemas/documentos_schema.py
@@ -6,8 +6,8 @@ from pydantic import BaseModel
 class DocumentosSchema(BaseModel):
     id: Optional[int] = None
     nome_doc: str
-    tipo: Optional[str]
-    origem: Optional[str]
+    tipo: str
+    setor_origem: Optional[str]
 
     class Config:
         orm_mode = True
