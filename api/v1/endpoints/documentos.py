1b29c13b39edd384ed11ff782c6b1c8c06bec53c
Alisson <alissonporpino.aps@gmail.com>
2024-07-05 15:22:41 -0300

    endpoints calculo

--- api/v1/endpoints/documentos.py
+++ api/v1/endpoints/documentos.py
@@ -12,35 +12,39 @@ from models.usuarios_model import UsuarioModel
 from schemas.documentos_schema import DocumentosSchema, DocumentosCreateSchema
 from core.deps import get_session, get_current_user
 
-from sqlalchemy import select, func
+from sqlalchemy import select, func, and_
 
 
 router = APIRouter()
 
-# Calculo do numero de registro.
-async def calcular_num_reg(db):
-    # Obtém o ano atual
-    ano_atual = datetime.now().year
+
+async def calcular_num_reg(db, tipo_doc: str):
     
-    # Consulta para contar o número de documentos
-    stmt = select(func.count()).select_from(DocumentosModel)
+    # Consulta para contar o número de documentos de um tipo específico no ano atual
+    stmt = select(func.count()).where(and_(DocumentosModel.tipo_doc == tipo_doc, DocumentosModel.tipo_doc == "Interno"))
     total_linhas = await db.scalar(stmt)
 
     # Formata o num_reg como três dígitos, seguido pelo ano atual
-    num_reg = f"{total_linhas + 1:03d}/{ano_atual}"
+    num_reg = f"{total_linhas + 1:03d}"
     return num_reg
 
 
 # POST Documentos
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=DocumentosSchema)
 async def post_documento(documento: DocumentosCreateSchema, usuario_logado:UsuarioModel = Depends(get_current_user), db: AsyncSession = Depends(get_session)):
-    num_reg_calculado = await calcular_num_reg(db)
+    num_reg_calculado = await calcular_num_reg(db, DocumentosModel.tipo_doc)
+
+    # Obtém o ano atual
+    ano_atual = datetime.now().year
+
+    num_reg = f"{num_reg_calculado}/{ano_atual}"
 
     novo_documento: DocumentosModel = DocumentosModel(
-        num_reg=num_reg_calculado, nome_doc=documento.nome_doc, tipo_doc=documento.tipo_doc, descricao=documento.descricao, setor_origem=documento.setor_origem, criador_id=usuario_logado.id_user)
+        num_reg=num_reg, nome_doc=documento.nome_doc, tipo_doc=documento.tipo_doc, descricao=documento.descricao, setor_origem=documento.setor_origem, criador_id=usuario_logado.id_user)
 
     db.add(novo_documento)
     await db.commit()
+    await db.refresh(novo_documento)
 
     return novo_documento
 
